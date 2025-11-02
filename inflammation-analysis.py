09fc57d07741210591805685c11385428cf61bb8
mlhu1u25 <mlhu1u25@soton.ac.uk>
2025-11-02 20:17:35 +0000

    Code style fixes

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -6,7 +6,9 @@ import argparse
 from inflammation import models, views
 
 
-def main(args):
+def main(
+        args
+        ):
     """The MVC Controller of the patient inflammation data system.
 
     The Controller is responsible for:
@@ -21,18 +23,24 @@ def main(args):
     for filename in InFiles:
         inflammation_data = models.load_csv(filename)
 
-        view_data = {'average': models.daily_mean(inflammation_data), 'max': models.daily_max(inflammation_data), 'min': models.daily_min(inflammation_data)}
+        view_data = {
+            'average': models.daily_mean(inflammation_data),
+            'max': models.daily_max(inflammation_data),
+            'min': models.daily_min(inflammation_data)
+        }
 
         views.visualize(view_data)
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='A basic patient inflammation data management system')
+        description='A basic patient inflammation data management system'
+        )
 
     parser.add_argument(
         'infiles',
         nargs='+',
-        help='Input CSV(s) containing inflammation series for each patient')
+        help='Input CSV(s) containing inflammation series for each patient'
+        )
 
     args = parser.parse_args()
 
