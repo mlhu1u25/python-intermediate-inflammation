70720dc28ef80665daa6d77428aef59998a35b7a
mlhu1u25 <mlhu1u25@soton.ac.uk>
2025-11-02 20:23:39 +0000

    Fixed the docstrings to show what they do, parameters and return

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,25 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    : param data: 2D Numpy array of inflammation data
+    : return: 1D Numpy array of mean inflammation per day
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+    : param data: 2D Numpy array of inflammation data
+    : return: 1D Numpy array of max inflammation per day
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+    : param data: 2D Numpy array of inflammation data
+    : return: 1D Numpy array of min inflammation per day
+    """
     return np.min(data, axis=0)
 
